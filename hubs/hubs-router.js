5c2154e481efe0c08865167dbc074cb56a9fe6be
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-01 20:37:07 -0400

    commit

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -150,3 +150,36 @@ async function validateId(req, res, next) {
 server.use((error, req, res, next) => {});
 
 module.exports = router;
+
+// Lecture Stuff
+
+// /*
+//   separate into controllers file and routers file
+// */
+
+// router.get('/', (req,res,next) => {
+//   console.log('first handler...');
+//   next();
+// }, (req,res,next) => {
+//   console.log('second handler...');
+//   next();
+// });
+
+// //controllers, store them in controller files
+// function handler1(req,res,next) {
+//   console.log('hanlder 1');
+//   next();
+// }
+// function handler1(req, res, next) {
+//   console.log('hanlder 2');
+//   next();
+// }
+
+// router.get('/', handler1, handler2);
+// router.get('/', handler1, handler2);
+// router.get('/', handler1, handler2);
+// router.get('/', handler1, handler2);
+// router.get('/', handler1, handler2);
+
+// const handlers = [handler1, handler2]
+// router.get('/', handlers);
